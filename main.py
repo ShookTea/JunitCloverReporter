ea3b110b8d737c828544b8cd3d949165d28d775c
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-03-27 10:19:18 +0200

    Reading files

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
 from github import Github
+import glob
 
 def main():
-    #token = os.environ["INPUT_GITHUB_TOKEN"]
-    #g = Github(token)
-    for key in os.environ:
-        print(key + " = " + os.environ[key])
-    #print("Hello from image!")
+    token = os.environ["INPUT_GITHUB_TOKEN"]
+    cloverGlob = os.environ["INPUT_CLOVER"]
+    junitGlob = os.environ["INPUT_JUNIT"]
+    repository = os.environ["GITHUB_REPOSITORY"]
+    commitSha = os.environ["GITHUB_SHA"]
+    workspace = os.environ["RUNNER_WORKSPACE"]
+
+    g = Github(token)
+
+    for file in os.listdir(workspace):
+        print(file)
 
 
 if __name__ == "__main__":
